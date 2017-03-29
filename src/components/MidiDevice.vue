0ceb45bbccfa2899e7efa2da27c5f7a3a851cc43
Mario Fink <mario.fink@goodyear.com>
2017-03-29 22:03:17 +0200

    playing midi notes is working

--- src/components/MidiDevice.vue
+++ src/components/MidiDevice.vue
@@ -1,21 +1,42 @@
 <template>
-  <h2>Midi Device</h2>
+  <div>
+    <h2>Midi Device</h2>
+    <div class="form-group">
+      <label for="midi-out">Midi output</label>
+      <select id="midi-out" name="midi-out" v-model="selectedOutput">
+        <option disabled value="">Please select one</option>
+        <option v-for="output in midiOutputs" :value="output.id">
+          {{ output.manufacturer }} {{ output.name }}
+        </option>
+      </select>
+      <button @click="playNote()">Play note</button>
+    </div>
+  </div>
 </template>
 
 <script>
   import WebMidi from "webmidi"
 
-  export default {
-    name: "midi-device",
-    data() {
-      return {
-        devices: null,
-      }
-    },
-    created() {
-      console.log("midi device", WebMidi)
-    },
+export default {
+  name: "midi-device",
+  data() {
+    return {
+      selectedOutput: "",
+      midiOutputs: null,
+    }
+  },
+  mounted() {
+    WebMidi.enable(() => {
+      this.midiOutputs = WebMidi.outputs
+    })
+  },
+  methods: {
+    playNote() {
+      const output = WebMidi.getOutputById(this.selectedOutput)
+      output.playNote("C3")
+    }
   }
+}
   </script>
 
   <style>
